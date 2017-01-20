7c148742fd0a556fbd24e8d8bd15c96da2415e25
cdaniels <cdaniels@techtonicgroup.com>
2017-01-20 16:14:15 -0700

    refactor

--- website.js
+++ website.js
@@ -2,7 +2,7 @@ $("#post-blog").click(function()
     {
         var newBlog = $("#new-blog-post").clone();
         var newBlogTitle = newBlog.find('#new-title');
-        newBlogTitle.text($('#new-blog-post').val())
-        $('#new-title').appendTo('#blog-title');
+        newBlogTitle.text($('#new-title').val())
+        $(newBlogTitle).prependTo('#blog-wrapper');
     }
 )
