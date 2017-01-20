5c457578164472b5ecacc8c08e1e78c5584ea9f0
cdaniels <cdaniels@techtonicgroup.com>
2017-01-20 16:34:08 -0700

    edits and the like

--- website.js
+++ website.js
@@ -2,7 +2,6 @@ $("#post-blog").click(function()
     {
         var newBlog = $("#new-blog-post").clone();
         var newBlogTitle = newBlog.find('#new-title');
-        newBlogTitle.text($('#new-title').val())
-        $(newBlogTitle).prependTo('#blog-wrapper');
+        $(newBlog).prependTo('#blog-wrapper');
     }
 )
