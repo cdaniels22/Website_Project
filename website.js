f35f338320fbce799e8ab472c3ac51efc3e57b66
Christopher Daniels <cdaniels@techtonicgroup.com>
2017-01-22 19:52:55 -0700

    no message

--- website.js
+++ website.js
@@ -1,7 +1,12 @@
-$("#post-blog").click(function()
-    {
-        var newBlog = $("#new-blog-post").clone();
-        var newBlogTitle = newBlog.find('#new-title');
-        $(newBlog).prependTo('#blog-wrapper');
-    }
-)
+$("#post-blog").click(function() {
+    var newBlogTitle = $('#new-title').val();
+    var newBlogAuthor = $('#new-author').val();
+    var newBlogDate = $('#new-date').val();
+    var newBlogContent = $('#new-content').val();
+    var newBlogPost = $("#blog-wrapper1").clone();
+    $(newBlogPost).find("#blog-title").text(newBlogTitle);
+    $(newBlogPost).find("#blog-author").text(newBlogAuthor);
+    $(newBlogPost).find("#blog-date").text(newBlogDate);
+    $(newBlogPost).find("#blog-content").text(newBlogContent);
+    $(newBlogPost).prependTo("#blog-wrapper1").first();
+});
