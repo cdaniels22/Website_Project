16f28d4dd2b555c9383ac46ce105f2e976dde8a6
cdaniels <cdaniels@techtonicgroup.com>
2017-01-23 12:32:23 -0700

    no message

--- website.js
+++ website.js
@@ -3,10 +3,13 @@ $("#post-blog").click(function() {
     var newBlogAuthor = $('#new-author').val();
     var newBlogDate = $('#new-date').val();
     var newBlogContent = $('#new-content').val();
+//    var newBlogImage = $('#blog-image').val('src');
     var newBlogPost = $("#blog-wrapper1").clone();
     $(newBlogPost).find("#blog-title").text(newBlogTitle);
     $(newBlogPost).find("#blog-author").text(newBlogAuthor);
     $(newBlogPost).find("#blog-date").text(newBlogDate);
     $(newBlogPost).find("#blog-content").text(newBlogContent);
+//    $(newBlogPost).find("#blog-image").text(newBlogImage);
     $(newBlogPost).prependTo("#blog-wrapper1").first();
+
 });
